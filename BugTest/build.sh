5dd43fccc5ce864f008679951aafb6d3380fabcb
Gair Shields <gair@interaxon.ca>
2025-02-04 22:09:38 -0500

    Updated to .net9.0

---
+++ BugTest/build.sh
@@ -0,0 +1,4 @@
+project="BugTest"
+framework="net9.0-maccatalyst"
+
+dotnet build $project.csproj -f $framework -c Release
---
+++ BugTest/run.sh
@@ -0,0 +1,4 @@
+project="BugTest"
+framework="net9.0-maccatalyst"
+
+dotnet run --project $project.csproj --no-launch-profile -f $framework -c Release
